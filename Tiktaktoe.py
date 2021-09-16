2b1c9df04bb9369447d8a33431068a1e10e64dc8
Mo Hijazi <mo.hijazi2@gmail.com>
2021-09-15 23:20:19 -0400

    Various changes to show how I would have done this better (incomplete)

--- Tiktaktoe.py
+++ Tiktaktoe.py
@@ -1,115 +1,72 @@
-# Start with grid- Grid should be a dictionary.
+# Start with grid- grid should be a dictionary.
 
-def TicTakToe(Current_Grid ,Turn ,Move, Spaces):
-    Grid = Current_Grid.replace(str(Move), Turn)
-    ###
-    Spaces[Move]= Turn  # If /1 or 0/ is needed
-    ###
-    print(Spaces[Move])
 
-    if Turn == "X":
-        Turn = "O"
-    else:
-        Turn = "X"
-    return Grid,Turn, Spaces
-####Used on Turn 5 and onwards
-def WinCondition(Spaces):
-   XRow1 = 0
-   XRow2 = 0
-   XRow3 = 0
-   YRow1 = 0
-   YRow2 = 0
-   YRow3 = 0
-   CrossL = 0
-   CrossR = 0
-   OXRow1 = 0
-   OXRow2 = 0
-   OXRow3 = 0
-   OYRow1 = 0
-   OYRow2 = 0
-   OYRow3 = 0
-   OCrossL = 0
-   OCrossR = 0
+# Used on turn 5 and onwards
 
-   for Key, Value in Spaces.items():## Itterates through each indidually
+
+def check_for_winner(grid):
+    # 1 2 3
+    # 4 5 6
+    # 7 8 9
+    # 1 4 7
+    # 2 5 8
+    # 3 6 9
+    # 1 5 9
+    # 3 5 7
+
+    if grid[1] == grid[2] and grid[2] == grid[3]:
+        return grid[1]
+    elif grid[4] == grid[5] and grid[5] == grid[6]:
+        return grid[4]
+    elif grid[7] == grid[8] and grid[8] == grid[9]:
+        return grid[7]
+    elif grid[7] == grid[8] and grid[8] == grid[9]:
+        return grid[7]
+    
+    return " "
+
+def print_grid(grid):
+    print(f"{grid[1]} | {grid[2]} | {grid[3]}")
+    print("-+-+-")
+    print(f"{grid[4]} | {grid[5]} | {grid[6]}")
+    print("-+-+-")
+    print(f"{grid[7]} | {grid[8]} | {grid[9]}")
+
+def execute_move(grid):
+    while True:
+        print_grid(grid)
         try:
-            int(Value)
+            move = int(input(("You are playing as", turn,
+                    "Select the space you want to fill:")))
         except (ValueError):
-            if Value == "X":
-                if Key in (1,2,3):
-                    XRow1 += 1
-                if Key in (4,5,6):
-                    XRow2 += 1
-                if Key in (7,8,9):
-                    XRow3 += 1
-                if Key in (1, 4, 7):
-                    YRow1 += 1
-                if Key in (2, 5, 8):
-                    YRow2 += 1
-                if Key in (3, 6, 9):
-                    YRow3 += 1
-                if Key in (1,5,9):
-                    CrossL += 1
-                if Key in (3,5,7):
-                    CrossR += 1
-                if 3 in (XRow1, XRow2, XRow3, YRow1, YRow2, YRow3, CrossL, CrossR):
-                    print("X wins!")
-                    quit()
+            print("Please Enter a Valid Number")
+        else:
+            if (move < 1) or (move > 9):
+                print("Please Enter a Valid Number")
+            elif grid[move] != " ":
+                print("That move has been played, try again.")
             else:
-                if Key in (1,2,3):
-                    OXRow1 += 1
-                if Key in (4,5,6):
-                    OXRow2 += 1
-                if Key in (7,8,9):
-                    OXRow3 += 1
-                if Key in (1, 4, 7):
-                    OYRow1 += 1
-                if Key in (2, 5, 8):
-                    OYRow2 += 1
-                if Key in (3, 6, 9):
-                    OYRow3 += 1
-                if Key in (1,5,9):
-                    OCrossL += 1
-                if Key in (3,5,7):
-                    OCrossR += 1
-                if 3 in (OXRow1, OXRow2, OXRow3, OYRow1, OYRow2, OYRow3, OCrossL, OCrossR):
-                    print("O wins!")
-                    quit()
+                grid[move] = turn
+                return
 
 #####################################################
-Spaces = {1:"1", 2:"2", 3:"3",
-           4:"4", 5:"5", 6:"6",
-           7:"7", 8:"8", 9:"9"}  # Used to record moves
+grid = {
+    1: " ", 2: " ", 3: " ", 
+    4: " ", 5: " ", 6: " ", 
+    7: " ", 8: " ", 9: " ", 
+}
 
-Grid="1|2|3\n4|5|6\n7|8|9" #Whats printed to the player
-
-Turn = "X" ## X goes first
-
+turn = "X"  # X goes first
+move_counter = 0  # move counter - Game ends at 9#this resets
 
 while True:
-    print(Grid)
-    while True: ### Gets the player to enter a valid move
-        try:
-            Move = int(input(("You are playing as", Turn, "Select the space you want to fill:")))
-        except (ValueError):
-            print("Please Enter a Valid Number")
-        if (Move >= 10) or (Move <= 0):
-            print("Please Enter a Valid Number")
-        elif (str(Spaces[Move]) != str(Move)):
-            print("That move has been played, try again.")
-        else:
-            break
+    execute_move(grid)
+    move_counter += 1
 
-    Grid, Turn, Spaces = TicTakToe(Grid,Turn,Move,Spaces)
-    GG= 0#move counter - Game ends at 9#this resets
-    for Key, Value in Spaces.items():
-        if str(Key) != str(Value):
-            GG += 1
-    if (GG >= 5) and (GG < 9):
-        print(Grid)
-        WinCondition(Spaces)
-    if (GG == 9):
-        print(Grid)
-        WinCondition(Spaces)
-        print("GG its a tie")
-        break
+    winner = check_for_winner(grid)
+    if winner != " ": 
+        print("Winner is :" + winner)
+    elif move_counter == 9:
+        print("Tie")
+    else: 
+        turn = "O" if turn == "X" else "X"
